005c7b4a7d0bf66dee2e81ceb7a10fa2a2809ce6
LeoCPeres <leonacsik@gmail.com>
2022-01-12 20:37:43 -0300

    API Routes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import { AppProps } from 'next/app'
 import '../../styles/global.scss'
 import { Header } from '../components/Header'
+import { SessionProvider } from 'next-auth/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <>
-    <Header />
-    <Component {...pageProps} />
+    <SessionProvider session={pageProps.session}>
+      <Header />
+      <Component {...pageProps} />
+    </SessionProvider>
   </>
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,8 +29,6 @@ export default function Home({ product }: HomeProps) {
           <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/images/avatar.svg" alt="Girl coding" />
-
-
       </main>
     </>
   )
