c2492eea9d5b0df25c5c10215833bed46c17054c
Leonardo Csik Peres <leonardocsikperes@gmail.com>
2022-07-13 11:46:43 -0300

    next_auth_secret

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,8 @@
-import { query as q } from 'faunadb'
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import { query as q } from "faunadb";
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
 
-import { fauna } from '../../../services/fauna'
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -12,14 +12,14 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       authorization: {
         params: {
-          scope: 'read:user',
-        }
-      }
+          scope: "read:user",
+        },
+      },
     }),
     // ...add more providers here
   ],
+  secret: process.env.NEXT_AUTH_SECRET,
   callbacks: {
-
     async signIn({ user, account, profile }) {
       const { email } = user;
 
@@ -28,25 +28,13 @@ export default NextAuth({
           q.If(
             q.Not(
               q.Exists(
-                q.Match(
-                  q.Index('user_by_email'),
-                  q.Casefold(user.email)
-                )
+                q.Match(q.Index("user_by_email"), q.Casefold(user.email))
               )
             ),
-            q.Create(
-              q.Collection('users'),
-              { data: { email } }
-            ),
-            q.Get(
-              q.Match(
-                q.Index('user_by_email'),
-                q.Casefold(user.email)
-              )
-            )
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email)))
           )
-        )
-
+        );
 
         return true;
       } catch {
@@ -60,31 +48,25 @@ export default NextAuth({
           q.Get(
             q.Intersection([
               q.Match(
-                q.Index('subscription_by_user_ref'),
+                q.Index("subscription_by_user_ref"),
                 q.Select(
                   "ref",
                   q.Get(
                     q.Match(
-                      q.Index('user_by_email'),
+                      q.Index("user_by_email"),
                       q.Casefold(session.user.email)
                     )
                   )
                 )
               ),
-              q.Match(
-                q.Index('subscription_by_status'),
-                "active"
-              )
+              q.Match(q.Index("subscription_by_status"), "active"),
             ])
           )
-        )
+        );
         return { ...session, activeSubscription: userActiveSubscription };
+      } catch (error) {
+        return { ...session, activeSubscription: null };
       }
-      catch (error) {
-        return { ...session, activeSubscription: null }
-
-      }
-
     },
-  }
-})
+  },
+});
